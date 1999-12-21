17572f13b31ebcbd602a814c4ea0fa00b2c7c570
gluk <>
1999-12-21 00:55:49 +0000

    *** empty log message ***

---
+++ src/ipstatd/src/daemon.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/param.h>
+#include <sys/resource.h>
+
+
+void daemon(void)
+{
+	int fd;
+	struct rlimit filelimit;
+
+	if (getppid() != 1){
+	  signal(SIGTTOU, SIG_IGN);
+	  signal(SIGTTIN, SIG_IGN);
+	  signal(SIGTSTP, SIG_IGN);
+	  
+	  if( fork() != 0)
+	    exit(0);
+	  setsid();
+	}
+
+	getrlimit(RLIMIT_NOFILE,&filelimit);
+	for( fd=0 ; fd<filelimit.rlim_max ; fd++ )
+	  close(fd);
+
+	chdir("/");
+}
