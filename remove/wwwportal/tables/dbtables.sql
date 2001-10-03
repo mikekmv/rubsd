d2a5f53cced482d462252e6b4dc4d2511beb323a
cruz <>
2001-10-03 13:18:59 +0000

    Some new features

--- remove/wwwportal/tables/dbtables.sql
+++ remove/wwwportal/tables/dbtables.sql
@@ -1,11 +1,40 @@
 
+DROP TABLE news;
 
 CREATE TABLE news (
-	id int(11),
-	author varchar2,
+	id serial,
+	author serial,
 	newsdate date,
 	topic varchar2,
 	intro varchar2,
 	body varchar2,
+	PRIMARY KEY(id)
+);
+
+DROP TABLE ftp;
+
+CREATE TABLE ftp (
+	id serial,
+	url varchar2,
+	desc varchar2,
+	PRIMARY KEY(id)
+);
+
+DROP TABLE ftpcontent;
+
+CREATE TABLE ftpcontent (
+	id serial,
+	ftp serial not null,
+	url varchar2,
+	chksum varchar2,
+	PRIMARY KEY(id)
+);
+
+DROP TABLE packetonftp;
+
+CREATE TABLE packetonftp (
+	id serial,
+	name varchar2,
+	ftpcontent serial,
 	PRIMARY KEY(id)
 );
