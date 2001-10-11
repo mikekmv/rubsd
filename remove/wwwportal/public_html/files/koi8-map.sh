24c6ffedb92bbaf1f0cf226007f900d903377bd3
cruz <>
2001-10-11 06:29:31 +0000

    Added section files

---
+++ remove/wwwportal/public_html/files/koi8-map.sh
@@ -0,0 +1,42 @@
+#
+# $RuOBSD: koi8-map.sh,v 1.1 2001/07/30 13:37:46 dfa Exp $
+# Original version from Igor Grabin
+#
+/sbin/wsconsctl -w map+='
+keycode 33  = f F Aacute aacute
+keycode 51  = comma less Acircumflex acircumflex
+keycode 32  = d D multiply division
+keycode 22  = u U Ccedilla ccedilla
+keycode 38  = l L Adiaeresis adiaeresis
+keycode 20  = t T Aring aring
+keycode 53  = slash question sterling threesuperior
+keycode 39  = semicolon colon Odiaeresis odiaeresis
+keycode 25  = p P Uacute uacute
+keycode 48  = b B Eacute eacute
+keycode 16  = q Q Ecircumflex ecircumflex
+keycode 19  = r R Ediaeresis ediaeresis
+keycode 37  = k K Igrave igrave
+keycode 47  = v V Iacute iacute
+keycode 21  = y Y Icircumflex icircumflex
+keycode 36  = j J Idiaeresis idiaeresis
+keycode 34  = g G ETH eth
+keycode 35  = h H Ograve ograve
+keycode 46  = c C Oacute oacute
+keycode 49  = n N Ocircumflex ocircumflex
+keycode 18  = e E Otilde otilde
+keycode 30  = a A AE ae
+keycode 26  = bracketleft braceleft Egrave egrave
+keycode 17  = w W Atilde atilde
+keycode 45  = x X THORN thorn
+keycode 23  = i I Ucircumflex ucircumflex
+keycode 24  = o O Yacute yacute
+keycode 27  = bracketright braceright ssharp ydiaeresis
+keycode 31  = s S Ugrave ugrave
+keycode 50  = m M Ooblique oslash
+keycode 40  = apostrophe quotedbl Udiaeresis udiaeresis
+keycode 52  = period greater Agrave agrave
+keycode 44  = z Z Ntilde ntilde
+keycode 7  = 6 asciicircum 6 comma
+keycode 8  = 7 ampersand 7 period
+keycode 184 = Mode_switch'
+
