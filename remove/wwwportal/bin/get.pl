20cea12d3e21da811d6e2df22fa714830f86cff5
null <>
2001-11-19 12:18:48 +0000

    cruz, thanks for tips :) please, check this one :) if you have got an errors, let me know :)

--- remove/wwwportal/bin/get.pl
+++ remove/wwwportal/bin/get.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
 # We need to put this script to crontab
 # descr: get.pl is used to fork scripts, nothing more :)
 # mailto: null@openbsd.ru
@@ -6,35 +6,41 @@
 use lib '/home/mil/cvs/wwwportal/bin';
 use main;
 
-$scripts = "/home/mil/cvs/wwwportal/bin/scripts"; 
-
--d $scripts || die "$!: \"$scripts\"\n"; 
-
-$SIG{CHLD}='IGNORE'; # Avoid zombie
+$dir = "/home/mil/cvs/wwwportal/bin/scripts"; 
+-d $dir || die "$!: \"$dir\"\n"; 
 
 &log("Started news tool");
 
 my $i=0;
-foreach (<$scripts/*.pl>)
+opendir(SCRIPTS, "$dir") || die "Can't open directory $dir\n";
+while( my $script = readdir(SCRIPTS) )
 {
-	chomp;
-	my $script = $_;
-	++$i;
+	if( !(-d $script) && $script =~ m/\.pl$/g ) 
+	{
+		++$i;
+		my $filename = "$dir/$script";
 	
-	&log("Found script: $script");
-
-	unless (fork) {
-	  unless (fork) {
-		if ( system("/usr/bin/perl $script 2>&1 >/dev/null") != 0 ) { 
-			&log("$script failed!");
-		}
-		exit 0;
-	  }
-	exit 0;
+		&log("Found script: $filename");
+		&chld($filename);
 	}
 }
+closedir(SCRIPTS);
+
+exit;
 
 $i == 0 ? 
 	&log ("Finished. No scripts found.") : 
 	&log ("$i script(s) forked");
 exit;
+
+$SIG{CHLD}='IGNORE'; # Avoid zombie
+
+sub chld {
+	my $script = shift; return () if ( !(-f $script) );
+	unless (fork) {
+ 	 if ( system("/usr/bin/perl $script 2>&1 >/dev/null") != 0 ) { 
+		&log("$script failed!");
+	 }
+ 	 exit 0;
+	}
+}
