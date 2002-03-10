e69d5a2f388d498845c029bbb921c8dd17429c31
gluk <>
2002-03-10 09:45:59 +0000

    Ethernet support + buggy PPP support.

--- src/ipstatd/src/logpcap.c
+++ src/ipstatd/src/logpcap.c
@@ -4,17 +4,30 @@
 # include <config.h>
 #endif
 
-#if USE_PCAP
+#if HAVE_PCAP
 
-# if HAVE_PCAP_H
-#  include <pcap.h>
-# else
-#  if HAVE_PCAP_PCAP_H
-#   include <pcap/pcap.h>
-#  endif
+#if HAVE_PCAP_H
+# include <pcap.h>
+#else
+# if HAVE_PCAP_PCAP_H
+#  include <pcap/pcap.h>
 # endif
+#endif
+
+#include <netinet/if_ether.h>
+#include <net/ppp_defs.h>
 
 #include "ipstatd.h"
+
+#define SNAPLEN		128
+#define TMOUT		100		/* ms */
+#define IFNAME		"ppp0"
+
+char *ifname = IFNAME;
+
+char	errbuf[PCAP_ERRBUF_SIZE];
+pcap_t	*pcapd;
+int	link_type;
 
 void parse_pcap(u_char *, struct pcap_pkthdr *, u_char *);
 int  open_pcap(void);
@@ -23,13 +36,26 @@ void close_pcap(void);
 
 struct capture pcap_cap = { open_pcap, read_pcap, close_pcap };
 
-char	errbuf[PCAP_ERRBUFF_SIZE];
-pcap_t	*pcapd;
-
 int
 open_pcap()
 {
-	pcapd = pcap_open_live(ifname, 128, 0, 100, errbuf);
+	int snaplen = SNAPLEN;
+
+	pcapd = pcap_open_live(ifname, snaplen, 0, TMOUT, errbuf);
+	if (pcapd == NULL) {
+		syslog(LOG_ERR, "Failed to initialize: %s\n", errbuf);
+		return (-1);
+	}
+	link_type = pcap_datalink(pcapd);
+	if (link_type != DLT_EN10MB && link_type != DLT_PPP) {
+		syslog(LOG_ERR, "Unsupported datalink type\n");
+		pcap_close(pcapd);
+		return (-1);
+	}
+	snaplen = pcap_snapshot(pcapd);
+	syslog(LOG_NOTICE, "Listening on %s, snaplen %d\n", ifname, snaplen);
+
+	return (0);
 }
 
 void
@@ -43,27 +69,39 @@ read_pcap(void)
 	return;
 }
 
-/* XXX: Not yet */
 void
 parse_pcap(u_char *ptr, struct pcap_pkthdr *pcaphdr, u_char *pkt)
 {
 	struct packdesc	 pack;
-	struct pfloghdr	*pflog;
+	struct ip	*ip;
+	int		 hdr_size;
 
-	pflog = (struct pfloghdr*)pkt;
 
-        pack.ip = (struct ip *)((char *)pflog + sizeof(struct pfloghdr));
-	pack.plen = pcaphdr->caplen;
+	switch (link_type) {
+	case DLT_EN10MB:
+		hdr_size = sizeof(struct ether_header);
+		break;
+	case DLT_PPP:
+		hdr_size = PPP_HDRLEN;
+		break;
+	default:
+		syslog(LOG_ERR, "Unsupported datalink type\n");
+		pcap_close(pcapd);
+		stop();
+		/* NOTREACHED */
+	}
 
+	pack.plen = pcaphdr->caplen - hdr_size;
+        ip = (struct ip *)(pkt + hdr_size);
+
+	pack.ip = ip;
+	ip->ip_len = ntohs(ip->ip_len);
+	ip->ip_off = ntohs(ip->ip_off);
+	
 	pack.flags = 0;
-	if (pflog->action == PF_PASS)
-		pack.flags |= P_PASS;
-	else
-		pack.flags |= P_BLOCK;
-	if (pflog->dir == PF_OUT)
-		pack.flags |= P_OUTPUT;
+	pack.flags |= P_PASS;
 	pack.count = 1;
-	strncpy(pack.ifname, (const char*)pflog->ifname, IFNAMSIZ);
+	strncpy(pack.ifname, ifname, IFNAMSIZ);
 	pack.ifname[IFNAMSIZ - 1] = '\0';
 
 	parse_ip(&pack);
@@ -80,4 +118,4 @@ close_pcap(void)
 	return;
 }
 
-#endif /* USE_PCAP */
+#endif /* HAVE_PCAP */
