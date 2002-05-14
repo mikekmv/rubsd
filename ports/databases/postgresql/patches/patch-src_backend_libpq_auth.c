30a8482fd5699e06ac6a5da0542c1b5f17cd0e3e
pvk <>
2002-05-14 11:45:08 +0000

    Для тех, кому нужно и кто устал ждать :)

---
+++ ports/databases/postgresql/patches/patch-src_backend_libpq_auth.c
@@ -0,0 +1,27 @@
+--- src/backend/libpq/auth.c.orig	Thu May  2 02:44:44 2002
++++ src/backend/libpq/auth.c	Thu May  2 03:01:38 2002
+@@ -229,7 +229,7 @@
+ 				 " Kerberos error %d\n", retval);
+ 		com_err("postgres", retval,
+ 				"while getting server principal for service %s",
+-				pg_krb_server_keyfile);
++				PG_KRB_SRVNAM);
+ 		krb5_kt_close(pg_krb5_context, pg_krb5_keytab);
+ 		krb5_free_context(pg_krb5_context);
+ 		return STATUS_ERROR;
+@@ -283,8 +283,15 @@
+ 	 *
+ 	 * I have no idea why this is considered necessary.
+ 	 */
++#if defined(HAVE_KRB5_TICKET_ENC_PART2)
+ 	retval = krb5_unparse_name(pg_krb5_context,
+ 							   ticket->enc_part2->client, &kusername);
++#elif defined(HAVE_KRB5_TICKET_CLIENT)
++	retval = krb5_unparse_name(pg_krb5_context,
++							   ticket->client, &kusername);
++#else
++#error "bogus configuration"
++#endif
+ 	if (retval)
+ 	{
+ 		snprintf(PQerrormsg, PQERRORMSG_LENGTH,
---
+++ ports/databases/postgresql/patches/patch-src_interfaces_libpq_fe-auth.c
@@ -0,0 +1,30 @@
+--- src/interfaces/libpq/fe-auth.c.orig	Mon Nov  5 20:46:37 2001
++++ src/interfaces/libpq/fe-auth.c	Thu May  2 05:16:58 2002
+@@ -403,9 +403,18 @@
+ 	{
+ 		if (retval == KRB5_SENDAUTH_REJECTED && err_ret)
+ 		{
++#if defined(HAVE_KRB5_ERROR_TEXT_DATA)
+ 			snprintf(PQerrormsg, PQERRORMSG_LENGTH,
+ 			  libpq_gettext("Kerberos 5 authentication rejected: %*s\n"),
+ 					 err_ret->text.length, err_ret->text.data);
++#elif defined(HAVE_KRB5_ERROR_E_DATA)
++			snprintf(PQerrormsg, PQERRORMSG_LENGTH,
++			  libpq_gettext("Kerberos 5 authentication rejected: %*s\n"),
++					 err_ret->e_data->length,
++					 (const char *)err_ret->e_data->data);
++#else
++#error "bogus configuration"
++#endif
+ 		}
+ 		else
+ 		{
+@@ -683,7 +692,7 @@
+ char *
+ fe_getauthname(char *PQerrormsg)
+ {
+-	char	   *name = (char *) NULL;
++	const char *name = (char *) NULL;
+ 	char	   *authn = (char *) NULL;
+ 	MsgType		authsvc;
+ 
