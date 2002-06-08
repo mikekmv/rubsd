98e9209a7c330d8241e973c0e3bdc4fe8398ed49
gluk <>
2002-06-08 12:53:15 +0000

    sync with -current.

--- src/atactl/atactl.c
+++ src/atactl/atactl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: atactl.c,v 1.13 2002/06/04 23:08:44 csapuntz Exp $	*/
+/*	$OpenBSD: atactl.c,v 1.15 2002/06/08 12:50:42 gluk Exp $	*/
 /*	$NetBSD: atactl.c,v 1.4 1999/02/24 18:49:14 jwise Exp $	*/
 
 /*-
@@ -529,9 +529,9 @@ device_identify(argc, argv)
 		swap16_multi((u_int16_t *)(inqbuf->atap_model),
 		    sizeof(inqbuf->atap_model) / 2);
 		swap16_multi((u_int16_t *)(inqbuf->atap_serial),
-		    sizeof(inqbuf->atap_model) / 2);
-		swap16_multi((u_int16_t *)(inqbuf->atap_model),
-		    sizeof(inqbuf->atap_model) / 2);
+		    sizeof(inqbuf->atap_serial) / 2);
+		swap16_multi((u_int16_t *)(inqbuf->atap_revision),
+		    sizeof(inqbuf->atap_revision) / 2);
 	}
 
 	/*
