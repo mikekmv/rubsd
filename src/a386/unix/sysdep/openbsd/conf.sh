68fea08cde813a16063dcaf105a417bd278ce6c5
grange <>
2003-05-27 10:42:42 +0000

    Trivial configure script, stolen from linux port

---
+++ src/a386/unix/sysdep/openbsd/conf.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+host="$*"
+
+rm -f arch ../../include/a386/a386_reg.h ../os ../../sysdep.c
+rm -f ../../sysdep.h ../../include/a386/savecontext.h
+
+case "$host" in
+i?86-*)
+    ln -s i386 arch
+    ln -s ../../../i386/a386_reg.h ../../include/a386/a386_reg.h
+    ;;
+*)
+    echo unsupported cpu architecture
+    ;;
+esac
+
+ln -s openbsd ../os
+ln -s sysdep/openbsd/sysdep.c ../../sysdep.c
+ln -s sysdep/openbsd/arch/arch.h ../../sysdep.h
+ln -s ../../sysdep/openbsd/arch/savecontext.h ../../include/a386/savecontext.h
