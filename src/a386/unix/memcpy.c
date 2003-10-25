6dbb1686c8635081e3b4174329dc5932cf413879
grange <>
2003-10-25 10:49:49 +0000

    Don't think I'll return to it...

--- src/a386/unix/memcpy.c
+++
@@ -1,9 +0,0 @@
-void
-memcpy (void *destination, void *source, int length)
-{
-  /* naive implementation */
-  int i;
-
-  for (i = 0; i < length; i++)
-    *(char *)destination++ = *(char *)source++;
-}
