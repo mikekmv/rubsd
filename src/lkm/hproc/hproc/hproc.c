f1609f5b1932ced86e9e6bc5060d850ed0c2432d
form <>
2004-10-27 06:20:32 +0000

    Oohh! Aahh! Shit happens! We should call lkmexists() manually...

--- src/lkm/hproc/hproc/hproc.c
+++ src/lkm/hproc/hproc/hproc.c
@@ -1,4 +1,4 @@
-/*	$RuOBSD: hproc.c,v 1.3 2004/04/13 14:46:09 form Exp $	*/
+/*	$RuOBSD: hproc.c,v 1.4 2004/06/10 10:44:54 form Exp $	*/
 
 /*
  * Copyright (c) 2004 Oleg Safiullin <form@pdp-11.org.ru>
@@ -46,6 +46,7 @@
 
 #include "hproc.h"
 
+extern int lkmexists(struct lkm_table *);
 
 static int hproc_load(struct lkm_table *, int);
 int hproc_lkmentry(struct lkm_table *, int, int);
@@ -98,6 +99,8 @@ hproc_load(struct lkm_table *lkmtp, int cmd)
 
 	switch (cmd) {
 	case LKM_E_LOAD:
+		if (lkmexists(lkmtp))
+			return (EEXIST);
 		error = kthread_create(hproc_kthread, &_module, NULL, "hprocd");
 		if (error != 0)
 			return (error);
