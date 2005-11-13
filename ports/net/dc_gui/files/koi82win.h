de37da9d6b5c75bc65962da0ce51210b9e7d0f31
grange <>
2005-11-13 18:13:34 +0000

    A horribly hacked version of the dc_gui with a hardcoded win<->koi8 translation for all text strings. Very useful to find some pron in a Windows Direct Connect P2P net.

---
+++ ports/net/dc_gui/files/koi82win.h
@@ -0,0 +1,46 @@
+/*-
+ *
+ * Copyright (C) 1997-98 by Andrey A. Chernov, Moscow, Russia
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+char *
+koi82win[] = {
+   "-",    "|",    "ã",    "¬",    "L",    "-",    "+",    "+", /* 80-87 */
+   "T",    "+",    "+",    "-",    "-",    "#",    "|",    "|", /* 88-8F */
+   "#",    "#",    "#",    "?",    "|", "\x95",    "v",    "=", /* 90-97 */
+   "<",    ">", "\xA0",    "?", "\xB0",    "2", "\xB7",    "+", /* 98-9F */
+   "=",    "|",    "ã", "\xB8",    "ã",    "ã",    "¬",    "¬", /* A0-A7 */
+   "¬",    "L",    "L",    "L",    "-",    "-",    "-",    "|", /* A8-AF */
+   "|",    "|",    "|", "\xA8",    "|",    "|",    "T",    "T", /* B0-B7 */
+   "T",    "-",    "-",    "-",    "+",    "+",    "+", "\xA9", /* B8-BF */
+"\xFE", "\xE0", "\xE1", "\xF6", "\xE4", "\xE5", "\xF4", "\xE3", /* C0-C7 */
+"\xF5", "\xE8", "\xE9", "\xEA", "\xEB", "\xEC", "\xED", "\xEE", /* C8-CF */
+"\xEF", "\xFF", "\xF0", "\xF1", "\xF2", "\xF3", "\xE6", "\xE2", /* D0-D7 */
+"\xFC", "\xFB", "\xE7", "\xF8", "\xFD", "\xF9", "\xF7", "\xFA", /* D8-DF */
+"\xDE", "\xC0", "\xC1", "\xD6", "\xC4", "\xC5", "\xD4", "\xC3", /* E0-E7 */
+"\xD5", "\xC8", "\xC9", "\xCA", "\xCB", "\xCC", "\xCD", "\xCE", /* E8-EF */
+"\xCF", "\xDF", "\xD0", "\xD1", "\xD2", "\xD3", "\xC6", "\xC2", /* F0-F7 */
+"\xDC", "\xDB", "\xC7", "\xD8", "\xDD", "\xD9", "\xD7", "\xDA"  /* F8-FF */
+};
---
+++ ports/net/dc_gui/files/win2koi8.h
@@ -0,0 +1,46 @@
+/*-
+ *
+ * Copyright (C) 1997-98 by Andrey A. Chernov, Moscow, Russia
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+char *
+win2koi8[] = {
+   "ÿ",    "ח",    "\'",   "ַ",    "\"",   "...",  "+",    "+", /* 80-87 */
+   "#",    "%",    "לר",   "<",    "מר",   "כ",    "h",    "ד", /* 88-8F */
+   "h",    "`",    "\'",   "\"",   "\"", "\x95",   "-",    "-", /* 90-97 */
+   "#",    "(TM)", "ּ״",   ">",    "־״",   "ֻ",    "h",    "ֳ", /* 98-9F */
+"\x9A",    "ץ",    "ױ",    "J",    "$",    "ח",    "|",    "$", /* A0-A7 */
+"\xB3", "\xBF",    "ו",    "\"",   "^",    "-",    "(R)",  "I", /* A8-AF */
+"\x9C",    "+",    "I",    "i",    "ַ",    "m",    "#", "\x9E", /* B0-B7 */
+"\xA3",    "N",    "ֵ",    "\"",   "j",    "S",    "s",    "i", /* B8-BF */
+"\xE1", "\xE2", "\xF7", "\xE7", "\xE4", "\xE5", "\xF6", "\xFA", /* C0-C7 */
+"\xE9", "\xEA", "\xEB", "\xEC", "\xED", "\xEE", "\xEF", "\xF0", /* C8-CF */
+"\xF2", "\xF3", "\xF4", "\xF5", "\xE6", "\xE8", "\xE3", "\xFE", /* D0-D7 */
+"\xFB", "\xFD", "\xFF", "\xF9", "\xF8", "\xFC", "\xE0", "\xF1", /* D8-DF */
+"\xC1", "\xC2", "\xD7", "\xC7", "\xC4", "\xC5", "\xD6", "\xDA", /* E0-E7 */
+"\xC9", "\xCA", "\xCB", "\xCC", "\xCD", "\xCE", "\xCF", "\xD0", /* E8-EF */
+"\xD2", "\xD3", "\xD4", "\xD5", "\xC6", "\xC8", "\xC3", "\xDE", /* F0-F7 */
+"\xDB", "\xDD", "\xDF", "\xD9", "\xD8", "\xDC", "\xC0", "\xD1"  /* F8-FF */
+};
